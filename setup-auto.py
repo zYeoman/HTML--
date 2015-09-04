e5d31ff61adb3b026da398de936ca4f4820c864f
Yongwen Zhuang <zyeoman@163.com>
2015-09-04 17:37:38 +0800

    delete all things which use python

--- setup-auto.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-import py2exe
-setup(
-    version="7",
-    zipfile=None,
-    console=['auto.py'],
-    options={"py2exe": {"includes": ["sip"]}}
-)
--- setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-import py2exe
-setup(console=['html.py'], version='7',
-      zipfile=None,
-      options={"py2exe": {"includes": ["sip"]}})
